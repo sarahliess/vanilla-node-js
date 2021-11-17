6ef7c83d883d16b6252d3856a5d0cac9b92e6bbd
Sarah Ließ <sarah.liess@hotmail.de>
2021-11-17 14:29:30 +0100

    comment for import statement

--- exercise4.js
+++ exercise4.js
@@ -1,5 +1,10 @@
-import fs from "fs";
-// const fs = require("fs");
+//In order to make import work, either: add "type": "module" to your package.json and run npm start (don't forget to define start in scripts) or change .js into .mjs file
+
+// import fs from "fs";
+
+
+
+const fs = require("fs");
 
 fs.readFile("test.html", "utf8", (err, data) => {
   if (err) throw err;
